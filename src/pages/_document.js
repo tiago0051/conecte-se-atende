513fbe0abe96996a970d9c02ba3a07a52cfd7563
Tiago Salgado <tiagosalgado2002@hotmail.com>
2021-12-28 17:55:41 -0300

    Favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;700&display=swap" rel="stylesheet" />
+                <link rel="icon" type="image/svg+xml" href="/logo-laranja.svg"></link>
             </Head>
             <body>
                 <Main />
