337438a2e0506b66b7844f8c943e27037a99e9b2
Tiago Salgado <tiagosalgado2002@hotmail.com>
2022-01-06 17:51:12 -0300

    CORREÇÃO DE ERRO

--- src/services/db.ts
+++ src/services/db.ts
@@ -4,7 +4,7 @@ let connection: MySql.Connection;
 
 export default async function DB(){
     if(!(connection && connection.state === 'connected')){
-        connection = await MySql.createConnection("mysql://"+ process.env.PLANETSCALE_DB_USERNAME +":"+ process.env.PLANETSCALE_DB_PASSWORD +"@"+ process.env.PLANETSCALE_DB_HOST +"/"+ process.env.PLANETSCALE_DB_DATABASE +"?ssl=true");
+        connection = await MySql.createConnection("mysql://"+ process.env.PLANETSCALE_DB_USERNAME +":"+ process.env.PLANETSCALE_DB_PASSWORD +"@"+ process.env.PLANETSCALE_DB_HOST +"/"+ process.env.PLANETSCALE_DB +"?ssl=true");
     
         await connection.connect();
     }
