ad1f8c65a227bacec1737bfe34762344d61635a6
Tiago Salgado <tiagosalgado2002@hotmail.com>
2022-01-31 17:24:35 -0300

    Atualização GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,25 +35,42 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;700&display=swap" rel="stylesheet" />
-          <link rel="icon" type="image/svg+xml" href="/logo-laranja.svg"></link>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-1TZ59ZMCXH"
+          />
 
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-1TZ59ZMCXH', {
-                    page_path: window.location.pathname,
-                  });
-                `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-1TZ59ZMCXH', { page_path: window.location.pathname });
+              `,
             }}
           />
+
+          <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MH9NFRG');
+            `,
+              }}
+            />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;700&display=swap" rel="stylesheet" />
+          <link rel="icon" type="image/svg+xml" href="/logo-laranja.svg"></link>
         </Head>
         <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH9NFRG"
+        height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
           <Main/>
           <NextScript/>
         </body>
